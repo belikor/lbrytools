80a52e91312e5289b5bc55682853815477ec191a
belikor <belikor@users.noreply.github.com>
2023-01-18 23:07:28 -0600

    print_claims: new methods `get_fields` and `get_line`
    The first new method `get_fields` method collects the information
    from a claim and returns a dictionary and returns a dictionary
    where each value is a string that can be printed in a line.
    
    The parameter `long_chan` allows displaying the channel name as is (`False`),
    or padding it with whitespace (`True`), so that many subsequent lines
    are aligned.
    
    The parameter `title` allows choosing to display the claim `'name'`
    or claim `'title'`, if it exists.
    
    The parameter `sanitize` allows removing the emojis from
    the claim `'name'` or `'title'`, and the `'channel'` strings.
    This is necessary in some interface systems that can't display emojis
    properly.
    
    ----
    
    The new `get_line` method first uses `get_field` to get the fields,
    and then builds the text line that will be printed based on
    the switches that are passed to it, `long_chan`, `create`, `height`, `release`,
    `claim_id`, `typ`, `ch_name`, `sizes`, `fees`, `title`, and `sanitize`.
    
    ----
    
    The `print_sch_claims` method is restructured to avoid calculating
    the text fields. It cycles over the list of claims,
    calls `get_line` on each claim, and builds a `line` variable
    that is added to the list of lines `out`, which is finally printed
    by `funcs.print_content` at the end of the method.
    
    The `print_tr_claims` method is just the same `print_sch_claims`
    but sets `ch_name=True` and `long_chan=True` by default.

--- print_claims.py
+++ print_claims.py
@@ -32,154 +32,215 @@ import time
 import lbrytools.funcs as funcs
 
 
+def get_fields(claim,
+               long_chan=False,
+               title=False, sanitize=False):
+    """Common code to get information to print."""
+    meta = claim["meta"]
+    value = claim["value"]
+
+    create_height = meta.get("creation_height", 0)
+    create_height = f"{create_height:8d}"
+
+    create_time = meta.get("creation_timestamp", 0)
+    create_time = time.strftime(funcs.TFMTp, time.gmtime(create_time))
+
+    block_height = claim["height"]
+    block_height = f"{block_height:8d}"
+
+    timestamp = claim["timestamp"]
+    timestamp = time.strftime(funcs.TFMTp, time.gmtime(timestamp))
+
+    rels_time = int(value.get("release_time", 0))
+
+    if not rels_time:
+        rels_time = create_time[:]
+    else:
+        rels_time = time.strftime(funcs.TFMTp, time.gmtime(rels_time))
+
+    vtype = claim["value_type"]
+    vtype = f"{vtype:10s}"
+
+    stream_type = value.get("stream_type", 8 * "_")
+    stream_type = f"{stream_type:9s}"
+
+    mtype = 14 * "_"
+
+    if "source" in value:
+        mtype = value["source"].get("media_type", 14 * "_")
+
+    mtype = f"{mtype:17s}"
+
+    channel = 14 * "_"
+
+    if "signing_channel" in claim:
+        if "canonical_url" in claim["signing_channel"]:
+            channel = claim["signing_channel"]["canonical_url"]
+            channel = channel.split("lbry://")[1]
+        elif "permanent_url" in claim["signing_channel"]:
+            channel = claim["signing_channel"]["permanent_url"]
+            _ch, _id = channel.split("#")
+            _ch = _ch.split("lbry://")[1]
+            channel = _ch + "#" + _id[0:3]
+        else:
+            channel = 14 * "_"
+
+    if long_chan:
+        channel = f"{channel:40s}"
+    else:
+        channel = f"{channel}"
+
+    seconds = 0
+
+    if "video" in value and "duration" in value["video"]:
+        seconds = value["video"]["duration"]
+    if "audio" in value and "duration" in value["audio"]:
+        seconds = value["audio"]["duration"]
+
+    mi = seconds // 60
+    sec = seconds % 60
+    duration = f"{mi:3d}:{sec:02d}"
+
+    size = 0
+
+    if "source" in value and "size" in value["source"]:
+        size = float(value["source"]["size"])
+
+    size_mb = size / (1024**2)  # to MB
+    size_mb = f"{size_mb:9.4f} MB"
+
+    fee = " "
+
+    if "fee" in value:
+        fee = value["fee"].get("amount", "___")
+        fee = f"{fee} " + value["fee"]["currency"]
+
+    fee = f"f: {fee:>9}"
+
+    name = claim["name"]
+
+    if title:
+        name = value.get("title") or name
+
+    if sanitize:
+        name = funcs.sanitize_text(name)
+        channel = funcs.sanitize_text(channel)
+
+    name = f'"{name}"'
+
+    return {"meta": meta,
+            "value": value,
+            "create_height": create_height,
+            "create_time": create_time,
+            "block_height": block_height,
+            "timestamp": timestamp,
+            "release_time": rels_time,
+            "value_type": vtype,
+            "stream_type": stream_type,
+            "media_type": mtype,
+            "channel": channel,
+            "duration": duration,
+            "size_mb": size_mb,
+            "fee": fee,
+            "name": name}
+
+
+def get_line(claim,
+             create=False, height=False, release=True,
+             claim_id=False, typ=True, ch_name=True,
+             long_chan=True,
+             sizes=True, fees=True,
+             title=False, sanitize=False,
+             sep=";"):
+    """Get a line to print information for a single claim."""
+    fields = get_fields(claim, long_chan=long_chan,
+                        title=title, sanitize=sanitize)
+
+    create_height = fields["create_height"]
+    create_time = fields["create_time"]
+    block_height = fields["block_height"]
+    timestamp = fields["timestamp"]
+    rels_time = fields["release_time"]
+
+    vtype = fields["value_type"]
+    stream_type = fields["stream_type"]
+    mtype = fields["media_type"]
+
+    channel = fields["channel"]
+
+    duration = fields["duration"]
+    size_mb = fields["size_mb"]
+    fee = fields["fee"]
+
+    name = fields["name"]
+
+    line = ""
+
+    if create:
+        line += f"{create_height}" + f"{sep} "
+        line += f"{create_time}" + f"{sep} "
+
+    if height:
+        line += f"{block_height}" + f"{sep} "
+        line += f"{timestamp}" + f"{sep} "
+
+    if release:
+        line += f"{rels_time}" + f"{sep} "
+
+    if claim_id:
+        line += claim["claim_id"] + f"{sep} "
+
+    if typ:
+        line += f"{vtype}" + f"{sep} "
+        line += f"{stream_type}" + f"{sep} "
+        line += f"{mtype}" + f"{sep} "
+
+    if ch_name:
+        line += f"{channel}" + f"{sep} "
+
+    if sizes:
+        line += f"{duration}" + f"{sep} "
+        line += f"{size_mb}" + f"{sep} "
+
+    if fees:
+        line += f"{fee}" + f"{sep} "
+
+    line += f"{name}"
+
+    return line
+
+
 def print_tr_claims(claims,
                     create=False, height=False, release=True,
                     claim_id=False, typ=True, ch_name=True,
+                    long_chan=True,
                     sizes=True, fees=True,
                     title=False, sanitize=False,
                     start=1, end=0,
                     reverse=False,
                     file=None, fdate=False, sep=";"):
     """Print generic claims, particularly trending or searched claims."""
-    n_claims = len(claims)
-
-    if reverse:
-        claims.reverse()
-
-    out = []
-
-    for num, claim in enumerate(claims, start=1):
-        if num < start:
-            continue
-        if end != 0 and num > end:
-            break
-
-        meta = claim["meta"]
-        value = claim["value"]
-
-        create_height = meta.get("creation_height", 0)
-        create_height = f"{create_height:8d}"
-
-        create_time = meta.get("creation_timestamp", 0)
-        create_time = time.strftime(funcs.TFMTp, time.gmtime(create_time))
-
-        block_height = claim["height"]
-        block_height = f"{block_height:8d}"
-
-        timestamp = claim["timestamp"]
-        timestamp = time.strftime(funcs.TFMTp, time.gmtime(timestamp))
-
-        rels_time = int(value.get("release_time", 0))
-
-        if not rels_time:
-            rels_time = create_time
-        else:
-            rels_time = time.strftime(funcs.TFMTp, time.gmtime(rels_time))
-
-        vtype = claim["value_type"]
-
-        stream_type = value.get("stream_type", 8 * "_")
-
-        if "source" in value:
-            mtype = value["source"].get("media_type", 14 * "_")
-        else:
-            mtype = 14 * "_"
-
-        if "signing_channel" in claim:
-            if "canonical_url" in claim["signing_channel"]:
-                channel = claim["signing_channel"]["canonical_url"]
-                channel = channel.split("lbry://")[1]
-            else:
-                channel = claim["signing_channel"]["permanent_url"]
-                _ch, _id = channel.split("#")
-                _ch = _ch.split("lbry://")[1]
-                channel = _ch + "#" + _id[0:3]
-        else:
-            channel = 14 * "_"
-
-        seconds = 0
-
-        if "video" in value and "duration" in value["video"]:
-            seconds = value["video"]["duration"]
-        if "audio" in value and "duration" in value["audio"]:
-            seconds = value["audio"]["duration"]
-
-        mi = seconds // 60
-        sec = seconds % 60
-        duration = f"{mi:3d}:{sec:02d}"
-
-        size = 0
-
-        if "source" in value and "size" in value["source"]:
-            size = float(value["source"]["size"])
-
-        size_mb = size / (1024**2)  # to MB
-        size_mb = f"{size_mb:9.4f} MB"
-
-        if "fee" in value:
-            fee = value["fee"].get("amount", "___")
-            fee = f"{fee} " + value["fee"]["currency"]
-        else:
-            fee = " "
-
-        fee = f"f: {fee:>9}"
-
-        name = claim["name"]
-
-        if title:
-            name = value.get("title") or name
-
-        if sanitize:
-            name = funcs.sanitize_text(name)
-            channel = funcs.sanitize_text(channel)
-
-        line = f"{num:4d}/{n_claims:4d}" + f"{sep} "
-
-        if create:
-            line += f"{create_height}" + f"{sep} "
-            line += f"{create_time}" + f"{sep} "
-
-        if height:
-            line += f"{block_height}" + f"{sep} "
-            line += f"{timestamp}" + f"{sep} "
-
-        if release:
-            line += f"{rels_time}" + f"{sep} "
-
-        if claim_id:
-            line += claim["claim_id"] + f"{sep} "
-
-        if typ:
-            line += f"{vtype:10s}" + f"{sep} "
-            line += f"{stream_type:9s}" + f"{sep} "
-            line += f"{mtype:17s}" + f"{sep} "
-
-        if ch_name:
-            line += f"{channel:40s}" + f"{sep} "
-
-        if sizes:
-            line += f"{duration}" + f"{sep} "
-            line += f"{size_mb}" + f"{sep} "
-
-        if fees:
-            line += f"{fee}" + f"{sep} "
-
-        line += f'"{name}"'
-
-        out.append(line)
-
-    funcs.print_content(out, file=file, fdate=fdate)
+    print_sch_claims(claims,
+                     create=create, height=height, release=release,
+                     claim_id=claim_id, typ=typ, ch_name=ch_name,
+                     long_chan=long_chan,
+                     sizes=sizes, fees=fees,
+                     title=title, sanitize=sanitize,
+                     start=start, end=end,
+                     reverse=reverse,
+                     file=file, fdate=fdate, sep=sep)
 
 
 def print_sch_claims(claims,
                      create=False, height=False, release=True,
                      claim_id=False, typ=True, ch_name=False,
+                     long_chan=False,
                      sizes=True, fees=True,
                      title=False, sanitize=False,
                      start=1, end=0,
                      reverse=False,
                      file=None, fdate=False, sep=";"):
-    """Print the provided list of claims, particularly those from a channel."""
+    """Print the provided list of claims searched online."""
     n_claims = len(claims)
 
     if reverse:
@@ -193,113 +254,15 @@ def print_sch_claims(claims,
         if end != 0 and num > end:
             break
 
-        meta = claim["meta"]
-        value = claim["value"]
-
-        create_height = meta.get("creation_height", 0)
-        create_height = f"{create_height:8d}"
-
-        create_time = meta.get("creation_timestamp", 0)
-        create_time = time.strftime(funcs.TFMTp, time.gmtime(create_time))
-
-        block_height = claim["height"]
-        block_height = f"{block_height:8d}"
-
-        timestamp = claim["timestamp"]
-        timestamp = time.strftime(funcs.TFMTp, time.gmtime(timestamp))
-
-        rels_time = int(value.get("release_time", 0))
-        rels_time = time.strftime(funcs.TFMTp, time.gmtime(rels_time))
-
-        vtype = claim["value_type"]
-
-        stream_type = value.get("stream_type", 8 * "_")
-
-        if "source" in value:
-            mtype = value["source"].get("media_type", 14 * "_")
-        else:
-            mtype = 14 * "_"
-
-        if "signing_channel" in claim:
-            if "canonical_url" in claim["signing_channel"]:
-                channel = claim["signing_channel"]["canonical_url"]
-                channel = channel.split("lbry://")[1]
-            else:
-                channel = claim["signing_channel"]["permanent_url"]
-                _ch, _id = channel.split("#")
-                _ch = _ch.split("lbry://")[1]
-                channel = _ch + "#" + _id[0:3]
-        else:
-            channel = 14 * "_"
-
-        seconds = 0
-
-        if "video" in value and "duration" in value["video"]:
-            seconds = value["video"]["duration"]
-        if "audio" in value and "duration" in value["audio"]:
-            seconds = value["audio"]["duration"]
-
-        mi = seconds // 60
-        sec = seconds % 60
-        duration = f"{mi:3d}:{sec:02d}"
-
-        size = 0
-
-        if "source" in value and "size" in value["source"]:
-            size = float(value["source"]["size"])
-
-        size_mb = size / (1024**2)  # to MB
-        size_mb = f"{size_mb:9.4f} MB"
-
-        if "fee" in value:
-            fee = value["fee"].get("amount", "___")
-            fee = f"{fee} " + value["fee"]["currency"]
-        else:
-            fee = " "
-
-        fee = f"f: {fee:>9}"
-
-        name = claim["name"]
-
-        if title:
-            name = value.get("title") or name
-
-        if sanitize:
-            name = funcs.sanitize_text(name)
-            channel = funcs.sanitize_text(channel)
+        line_add = get_line(claim, long_chan=long_chan,
+                            create=create, height=height, release=release,
+                            claim_id=claim_id, typ=typ, ch_name=ch_name,
+                            sizes=sizes, fees=fees,
+                            title=title, sanitize=sanitize,
+                            sep=sep)
 
         line = f"{num:4d}/{n_claims:4d}" + f"{sep} "
-
-        if create:
-            line += f"{create_height}" + f"{sep} "
-            line += f"{create_time}" + f"{sep} "
-
-        if height:
-            line += f"{block_height}" + f"{sep} "
-            line += f"{timestamp}" + f"{sep} "
-
-        if release:
-            line += f"{rels_time}" + f"{sep} "
-
-        if claim_id:
-            line += claim["claim_id"] + f"{sep} "
-
-        if typ:
-            line += f"{vtype:10s}" + f"{sep} "
-            line += f"{stream_type:9s}" + f"{sep} "
-            line += f"{mtype:17s}" + f"{sep} "
-
-        if ch_name:
-            line += f"{channel}" + f"{sep} "
-
-        if sizes:
-            line += f"{duration}" + f"{sep} "
-            line += f"{size_mb}" + f"{sep} "
-
-        if fees:
-            line += f"{fee}" + f"{sep} "
-
-        line += f'"{name}"'
+        line += line_add
 
         out.append(line)
 
